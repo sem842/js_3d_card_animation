354f0f91574e446306a1246a7b618eb203174fa6
sem842 <gsem842@gmail.com>
2021-05-20 16:41:13 +0700

    Анимация появления

--- app/js/app.js
+++ app/js/app.js
@@ -39,4 +39,14 @@ const body = document.querySelector('body')
 		gsap.to('.content', 1, { transform: `rotate3d( ${tiltx}, ${tilty}, 0, ${degree}deg )`})
 	}
 
+	gsap.to('.card', { zoom: .98 })
+	gsap.to('.l_main', { opacity: 1, zoom: .98, duration: .1 })
+	gsap.to('.l2_main', { opacity: 1, left: -10, top: 10, duration: .25, delay: .25 })
+	gsap.to('.l3_main', { opacity: 1, left: -20, top: 20, duration: .25, delay: .25 })
+	gsap.to('.card-russia', { opacity: .07, duration: .1 })
+	gsap.to('.card-logo_w', { opacity: 1, duration: .225 })
+	gsap.to('.card-chip', { opacity: 1, duration: .225 })
+	gsap.to('.card-valid', { opacity: 1, zoom: 1, duration: .1, delay: .25 })
+	gsap.to('.card-number-holder', { opacity: 1, zoom: 1, duration: .1, delay: .25 })
+
 })
